f106d6050c19b6bec0c8cbe1edda2226c77bff1b
AlejandroFV <AlejandroFV@mail.com>
2015-04-15 23:44:28 -0500

    Se corrige código "spanglish"

--- login.php
+++ login.php
@@ -3,15 +3,17 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<link rel="stylesheet" href="styles/style.css">
+	<script type="text/javascript" src="scripts/session.js"></script>
 	<title>Car Rental System</title>
+
 </head>
-<body>
+<body onload="focusMouseLogIn();">
 <!-- Contenedor -->
 <div id="container">
 	<div id="header">
 		<div class="menu">
 			<ul id="menu">
-				<li class="space inline-block"><a href="..." class="pacifico verde">Inicio</a></li>
+				<li class="space inline-block"><a href="index.php" class="pacifico verde">Inicio</a></li>
 				<li class="space inline-block"><a href="..." class="pacifico">Perfil</a></li>
 				<li class="space inline-block"><a href="login.php" class="pacifico">Iniciar Sesión</a></li>
 				<li class="space inline-block"><a href="signup.php" class="pacifico">Registrarse</a></li>
@@ -19,32 +21,14 @@
 		</div>
 	</div>
 	<div id="body">
-		<div id="part-1">
-			<div id="cars-menu">
-				<ul>
-					<li class="cars-menu"><h3>Lista de vehiculos</h3></li>
-					<li class="cars-menu">auto1...</li>
-					<li class="cars-menu">auto2...</li>
-					<li class="cars-menu">auto3...</li>
-				</ul>
-			</div>
+		<div id="log-in">
+			<h3 class="title-one">Iniciar sesión</h3>
+			<form action="">
+				<label>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspUsuario:&nbsp<input class="combo-box" type="text" id="user"></label>
+				<br><label>Contraseña:<input class="combo-box" type="password" id="password"></label>
+				<br><label><input class="button" type="submit" value="Iniciar sesión" onclick="sendInfo();"></label>
+			</form>
 		</div>
-		<div id="part-2">
-			<div id="welcome">
-				<img src="images/welcome.png" class="welcomeImg">
-			</div>
-			<br><br>
-			<div id="promotion-1">
-				<img src="images/promocion1.png">
-				<p>Esta promoción es válida hasta octubre del 2015</p>
-			</div>
-			<div id="promotion-2">
-				<img src="images/promocion2.png">
-				<p>Esta promoción es válida hasta agotar existencias</p>
-			</div>
-		</div>
-		
-		
 	</div>
 	<div id="footer">
 		<div class="column-footer">
--- signup.php
+++ signup.php
@@ -3,7 +3,7 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<link rel="stylesheet" href="styles/style.css">
-	<script type="text/javascript" src="scripts/sesion.js"></script>
+	<script type="text/javascript" src="scripts/session.js"></script>
 	<title>Registrarse</title>
 
 </head>
@@ -13,27 +13,27 @@
 	<div id="header">
 		<div class="menu">
 			<ul id="menu">
-				<li class="espacio inline-block"><a href="index.php" class="pacifico verde">Inicio</a></li>
-				<li class="espacio inline-block"><a href="..." class="pacifico">Perfil</a></li>
-				<li class="espacio inline-block"><a href="sesion.php" class="pacifico">Iniciar Sesión</a></li>
-				<li class="espacio inline-block"><a href="registrarse.php" class="pacifico">Registrarse</a></li>
+				<li class="space inline-block"><a href="index.php" class="pacifico verde">Inicio</a></li>
+				<li class="space inline-block"><a href="..." class="pacifico">Perfil</a></li>
+				<li class="space inline-block"><a href="login.php" class="pacifico">Iniciar Sesión</a></li>
+				<li class="space inline-block"><a href="signup.php" class="pacifico">Registrarse</a></li>
 			</ul>
 		</div>
 	</div>
 	<div id="body">
-		<div id="registrarse">
+		<div id="sign-up">
 			<h3 class="title-one">Llene los datos</h3>
 			<form action="">
 				<label>
-					Escriba su nombre de usuario <br>
+					Escriba su nombre de usuario:<br>
 					<input type="text" id="name"> <br><br>
 				</label>
 				<label>
-					Escriba su contraseña <br>
+					Escriba su contraseña:<br>
 					<input type="password" id="pass"> <br><br>
 				</label>
 				<label>
-					Repita su contraseña su contraseña <br>
+					Repita su contraseña:<br>
 					<input type="password" id="pass"> <br><br>
 				</label>
 				<label>
@@ -44,13 +44,13 @@
 	</div>
 	<div id="footer">
 		<div class="column-footer">
-			<p>¿Quienes somos?</p>
+			<p>¿Quiénes somos?</p>
 		</div>
 		<div class="column-footer">
 			<p>Privacidad y Términos</p>
 		</div>
 		<div class="column-footer">
-			<p>Contactanos</p>
+			<p>Contáctanos</p>
 		</div>
 	</div>
 </div>
