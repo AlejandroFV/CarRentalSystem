0e0f3f876e7be482e9866580f8b851bc20846551
AlejandroFV <AlejandroFV@mail.com>
2015-05-04 23:16:56 -0500

    Modificación de la plantilla de conexión a BD
    Se implementa el uso de funciones de PHP MySQLi para la conexión a la
    base de datos.

--- layout/php/connection.php
+++ layout/php/connection.php
@@ -1,29 +1,24 @@
 <?php
 	/*
-	$DB_host = El host del servidor, en este caso localhost
-	$DB_user = El usuario
-	$DB_password = La contraseña
+		$DB_host = El host del servidor, en este caso localhost
+		$DB_user = El usuario
+		$DB_password = La contraseña
 	*/
 	$DB_host = "127.0.0.1";
 	$DB_user = "root";
 	$DB_password = "";
-	$DB_name = "CarRentalSystem";
+	$DB_name = "CRS";
 
-	/*
-	Init the connection
-	*/
-	$link = mysql_connect($DB_host, $DB_user, $DB_password)
-		or die("Could not connect to the database, ".mysql_error());
-	echo "Connected successfully";
+	// Init the connection
+	$con = mysqli_connect($DB_host, $DB_user, $DB_password);
+	// Check connection
+	if (mysqli_connect_errno()) {
+  		echo "Error en la conexión con MySQL: " . mysqli_connect_error();
+	}
 
-	/*
-	Select the data base
-	*/
-	mysql_select_db($DB_name)
-		or die("Couldn't select the database.");
+	// Select the data base
+	mysqli_select_db($con, $DB_name);
 
-	/*
-	Set the default character set for the current connection.
-	*/
-	mysql_query("SET NAMES 'utf8'");
+	// Set the default character set for the current connection.
+	mysqli_query($con, "SET NAMES 'utf8'");
 ?>
