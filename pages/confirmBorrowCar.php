e9d8ca22a89bfc61f34eeea7789fa73079f9b456
BuiRai <buirai_naetle@hotmail.com>
2015-05-22 23:34:46 -0500

    Reservación de autos lista
    Ahora ya puede reservar un auto el usuario. Esta reservación se guarda
    en la base de datos y a su vez, guarda la relación de la factura con el
    usuario.

--- pages/confirmBorrowCar.php
+++ pages/confirmBorrowCar.php
@@ -1,3 +1,7 @@
+<?php
+	session_start();
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -32,41 +36,82 @@
 		<div class="wrapper col5">
 			<div id="container">
 				<div id="content">
-					<h2>Escoge el auto que desees reservar</h2>
+					<h2>Información del auto que desea reservar</h2>
+					<form action="" method="post">
 					<table>
 
 						<!--Cabezera de la tabla-->
 						<thead>
 							<th>Tarifa diaria</th>
 							<th>Tarifa por hora</th>
-							<th>Color de auto</th>
+							<th>Color</th>
 							<th>Modelo de auto</th>
 							<th>Localización del auto</th>
-							<th>Reservar auto</th>
+							<th>Tipo de pago</th>
 						</thead>
 						<!--Fin de la cabezera de la tabla-->
 
 						<!--Cuerpo de la tabla-->
 						<tbody>
 							<?php
+								$idCar = $_GET['ID'];
 								require("../layout/php/connection.php");
-								$query = "SELECT * FROM car NATURAL JOIN cartype NATURAL JOIN locationcar NATURAL JOIN location";
-								$result = mysqli_query($con, $query);
+								$queryInformationCar = "SELECT * FROM car NATURAL JOIN cartype NATURAL JOIN locationcar NATURAL JOIN location 
+									WHERE id_car = $idCar";
+								$result = mysqli_query($con, $queryInformationCar);
 								while ($values = mysqli_fetch_array($result)) {
+									$tarifaPorDia = $values[3];
+									$tarifaPorHora = $values[4];
 									echo "<tr class='dark'>";
 									echo "<td>$values[3]</td>";
 									echo "<td>$values[4]</td>";
 									echo "<td>$values[5]</td>";
 									echo "<td>$values[6]</td>";
 									echo "<td>$values[7]</td>";
-									echo "<td><a href='confirmBorrowCar.php?ID= $values[1]'>Reservar</a></td>";
+									echo "<td>
+										      <select name='typePayment' id='typePayment'>
+											      <option value='Visa'>Visa</option>
+												  <option value='Visa Debit'>Visa Debit</option>
+												  <option value='Visa Electron'>Visa Electron</option>
+												  <option value='Mastercard'>Mastercard</option>
+												  <option value='American Express'>American Express</option>
+											  </select>
+										  </td>";
 									echo "</tr>";
 								}
+								echo "<h3>Usted pagara un total de <strong>$tarifaPorDia</strong> por la tarifa diaria o un total de <strong>$tarifaPorHora</strong> por la tarida por hora</h3>";
 							?>
+
 						</tbody>
 						<!--Fin del cuerpo de la table-->
-
 					</table>
+					<input type="submit" value="Confirmar Reservación" name="confirmReservation">
+					</form>
+
+					<!--Aqui es donde ya se realiza la transacción de la reservación de un automóvil-->
+					<?php
+						if (isset($_POST['confirmReservation'])) {
+							$nameUser = $_SESSION['nick_user'];
+							$hoy = date("y-m-d h:m:s");
+							$typePayment = $_POST['typePayment'];
+							
+							$queryInvoice = "INSERT INTO crs.invoice (date_invoice, type_payment, aval_invoice, total_cost) 
+								VALUES ('$hoy','$typePayment','$nameUser',$tarifaPorDia)";
+							require("../layout/php/connection.php");
+							mysqli_query($con, $queryInvoice);
+							echo "<script>alert('Has reservado un auto');</script>";
+
+							$queryInvoiceSelect = "SELECT * FROM crs.invoice WHERE date_invoice = '$hoy'";
+							$resultadoInvoiceSelect = mysqli_query($con, $queryInvoiceSelect);
+							$fila = mysqli_fetch_row($resultadoInvoiceSelect);
+							$id_invoice = $fila[0];
+
+							$id_user = $_SESSION["id_user"];
+							$queryServe = "INSERT INTO crs.serve(id_customer, id_invoice) VALUES ($id_user,$id_invoice)";
+							mysqli_query($con, $queryServe);
+							mysqli_close($con);
+						}
+					?>
 				</div>
 				<div id="column">
 					<div class="holder">
